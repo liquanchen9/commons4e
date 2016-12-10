4284f91cd90dd2a46126a6007062f11db55b479a
李全城 <398208587@qq.com>
2016-12-11 01:12:23 +0800

    Update CloneKeyHelper.java

--- org.jiayun.commons4e/src/org/jiayun/commons4e/handlers/menuhandles/CloneKeyHelper.java
+++ org.jiayun.commons4e/src/org/jiayun/commons4e/handlers/menuhandles/CloneKeyHelper.java
@@ -1,17 +1,29 @@
 package org.jiayun.commons4e.handlers.menuhandles;
 
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
+import java.lang.reflect.Field;
 
+import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ParameterizedCommand;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.handlers.IHandlerService;
 import org.jiayun.commons4e.handlers.MenuItemHandler;
 
 public class CloneKeyHelper extends MenuItemHandler{
-
+	
+	private IHandlerService handlerService;
+	private Command newEditorCommand;
+	
 	@Override
 	protected boolean init(ExecutionEvent event) throws Exception {
+		IWorkbenchWindow workbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
+		handlerService = workbenchWindow.getService(IHandlerService.class);
+		newEditorCommand = workbenchWindow.getService(ICommandService.class).getCommand("org.eclipse.ui.window.newEditor");
 		return true;
 	}
 
@@ -22,20 +34,21 @@ public class CloneKeyHelper extends MenuItemHandler{
 
 	@Override
 	protected void onClick(SelectionEvent event) throws Throwable {
-		Thread.sleep(100);
-		java.awt.Robot robot = new Robot();
-		robot.keyPress(KeyEvent.VK_ALT);
-		robot.keyRelease(KeyEvent.VK_ALT);
-		Thread.sleep(50);
-		robot.keyPress(KeyEvent.VK_W);
-		Thread.sleep(50);
-		robot.keyRelease(KeyEvent.VK_W);
-		robot.keyPress(KeyEvent.VK_E);
-		Thread.sleep(50);
-		robot.keyRelease(KeyEvent.VK_E);
-		robot.keyPress(KeyEvent.VK_E);
-		Thread.sleep(50);
-		robot.keyRelease(KeyEvent.VK_E);
-		
+		Event ev = new Event();
+		copyFields(event,ev);
+		this.handlerService.executeCommand(ParameterizedCommand.generateCommand(newEditorCommand, null), ev);
+	}
+
+	private void copyFields(Object src, Object target) {
+		Field[] fields = src.getClass().getFields();
+		Class<? extends Object> tc = target.getClass();
+		for (Field field : fields) {
+			try {
+				tc.getField(field.getName()).set(target, field.get(src));
+			} catch (NoSuchFieldException e) {
+			} catch (SecurityException e) {
+			} catch (IllegalArgumentException e) {
+			} catch (IllegalAccessException e) {}
+		}
 	}
 }
